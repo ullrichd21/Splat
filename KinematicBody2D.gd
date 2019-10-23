d23791e39289cbe6ba3c71e1bf58c4875ddac551
ullrichd21 <ullrichd@allegheny.edu>
2019-10-23 19:22:38 -0400

    Added ground and gravity

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,21 +1,43 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
+var motion = Vector2()
+var grav = 30
+var jmp = -600
+var mSpd = 400
+var jmpNum = 0
+const jmpMax = 2
+const UP = Vector2(0, -1)
 
-var velocity = Vector2()
-
-func get_input():
-    velocity = Vector2()
-    if Input.is_action_pressed('ui_right'):
-        velocity.x += 1
-    if Input.is_action_pressed('ui_left'):
-        velocity.x -= 1
-    if Input.is_action_pressed('ui_down'):
-        velocity.y += 1
-    if Input.is_action_pressed('ui_up'):
-        velocity.y -= 1
-    velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
-    get_input()
-    velocity = move_and_slide(velocity)
+	if Input.is_key_pressed(KEY_D) && !Input.is_key_pressed(KEY_A):
+		motion.x = mSpd
+		#$AnimatedSprite.flip_h = false
+		#$AnimatedSprite.play("Run")
+	elif Input.is_key_pressed(KEY_A) && !Input.is_key_pressed(KEY_D):
+		motion.x = -mSpd
+		#$AnimatedSprite.flip_h = true
+		#$AnimatedSprite.play("Run")
+	else:
+		motion.x = 0
+		#$AnimatedSprite.play("Idle")
+		
+	if Input.is_key_pressed(KEY_SPACE) && is_on_floor():
+		motion.y = jmp
+		jmpNum += 1
+	elif Input.is_key_pressed(KEY_SPACE) && !is_on_floor() && jmpNum < jmpMax:
+		motion.y = jmp
+		jmpNum += 1
+	
+	#if !is_on_floor() && motion.y > 0:
+		#$AnimatedSprite.play("Rise")
+	#elif !is_on_floor() && motion.y <= 0:
+		#$AnimatedSprite.play("Fall")
+	
+	if !is_on_floor():
+		motion.y += grav
+	else:
+		jmpNum = 0
+	
+	move_and_slide(motion, UP)
+	pass
